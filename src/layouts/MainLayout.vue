2330ffa0618e59d0f6e158e779399535c1e4ded5
Jamsimson <mwattanachant@gmail.com>
2022-11-14 22:28:00 +0700

    naphat

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primer text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar> </q-avatar>
+          TodoList
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
